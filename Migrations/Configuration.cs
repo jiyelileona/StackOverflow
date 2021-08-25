db258a7054b49c8340e44e59b62d31eb25b45526
Jiye Li <15832778446@163.com>
2021-08-25 10:31:09 -0500

    Add project files.

---
+++ Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace StackOverflow.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<StackOverflow.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(StackOverflow.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ Scripts/main.js
@@ -0,0 +1,11 @@
+﻿var tags = document.querySelector("#tags")
+var tagify = new Tagify(tags, {
+    originalInputValueFormat: valuesArr => valuesArr.map(item => item.value).join(',')
+})
+
+var comments = document.querySelectorAll(".add-comment")
+comments.forEach(c => c.addEventListener("click", e => {
+    e.preventDefault()
+    e.target.parentElement.querySelector("input").classList.toggle("comment")
+    e.target.parentElement.querySelector("button").classList.toggle("comment")
+}))
